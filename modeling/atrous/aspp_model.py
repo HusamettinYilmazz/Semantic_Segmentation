4a26346e2e6141f3d66620cc9197c9a82bd609f5
Husam <husamahmed616@gmail.com>
2026-04-07 17:51:50 +0300

    Add multi atrous with different rates to ASPP model

--- modeling/atrous/aspp_model.py
+++ modeling/atrous/aspp_model.py
@@ -11,6 +11,17 @@ class ASPP(nn.Module):
         self.resnet = models.resnet50(weights="DEFAULT")
         self.entry = nn.Sequential(*list(self.resnet.children())[:5])
         self.resnet = nn.Sequential(*list(self.resnet.children())[5:8])
-        
+        self.scales = nn.ModuleList()
+        for rate in rates:
+            network = nn.Sequential(
+                nn.Conv2d(in_channels= 2048, out_channels= 512,
+                          kernel_size= 3 if rate != 1 else 1, 
+                          padding= rate if rate != 1 else 0,
+                          dilation=rate),
+                          nn.BatchNorm2d(num_features=512), ## out_channels
+                          nn.ReLU()
+            )
+            self.scales.append(network)
+       
         # for child in self.entry:
         #     print(child)
